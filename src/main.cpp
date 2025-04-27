2be182e4c0ce1e11ee449d2fd4a7e2d7e0f1433b
yeisme <yefun2004@gmail.com>
2025-04-27 15:32:17 +0800

    vcpkg_base template demo, build with vcpkg

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include "rapidjson/document.h"
+#include <filesystem>
+#include <fstream>
+#include <string>
+using namespace rapidjson;
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    // read json to char* from file
+    auto file = ifstream("1.json");
+    cout << typeid(file).name() << endl;
+    if (!file.is_open())
+    {
+        cerr << "Error opening file" << endl;
+        return 1;
+    }
+    auto json = string((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+    cout << typeid(json).name() << endl;
+
+    auto document = Document();
+    cout << json << endl;
+    document.Parse(json.data());
+    cout << document.IsObject() << endl;
+
+    cout << document["pi"].GetFloat() << endl;
+    for (auto& v : document["a"].GetArray())
+    {
+        cout << v.GetInt();
+    }
+
     return 0;
 }
